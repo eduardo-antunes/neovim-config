2110094dccda97f9ccdd97b37d5852daf5712412
Eduardo Antunes dos Santos Vieira <eduardoantunes986@gmail.com>
2025-08-16 15:17:12 -0300

    Bom e velho catppuccin novamente

--- lua/eduardo/plugins/init.lua
+++ lua/eduardo/plugins/init.lua
@@ -4,21 +4,33 @@ local function gh(name)
   return string.format("https://github.com/%s", name)
 end
 
-local function src_hut(name)
-  return string.format("https://git.sr.ht/~%s", name)
-end
-
 vim.pack.add {
-  src_hut "poof/alabaster.nvim"        ,
-  gh "eduardo-antunes/plainline"       ,
-  gh "NMAC427/guess-indent.nvim"       ,
-  gh "nvim-treesitter/nvim-treesitter" ,
-  gh "echasnovski/mini.nvim"           ,
-  gh "stevearc/oil.nvim"               ,
+  { src = gh "catppuccin/nvim", name = "catppuccin" },
+  { src = gh "eduardo-antunes/plainline"            },
+  { src = gh "NMAC427/guess-indent.nvim"            },
+  { src = gh "nvim-treesitter/nvim-treesitter"      },
+  { src = gh "echasnovski/mini.nvim"                },
+  { src = gh "stevearc/oil.nvim"                    },
 }
 
-vim.g.alabaster_floatborder = true
-vim.cmd.colors "alabaster"
+require("catppuccin").setup {
+  no_bold = true,
+  show_end_of_buffer = true,
+  styles = { conditionals = {} },
+  custom_highlights = function(colors)
+    local none = { fg = colors.text }
+    return {
+      Constant = none,
+      ["@constant.macro"]     = none,
+      ["@constant.builtin"]   = none,
+      ["@function.builtin"]   = none,
+      ["@variable.builtin"]   = none,
+      ["@variable.parameter"] = none,
+      ["@function.call"]      = none,
+    }
+  end
+}
+vim.cmd.colors "catppuccin"
 
 require("plainline").setup()
 require("guess-indent").setup()
